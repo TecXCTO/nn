3d99e20317c25b30123758dc679dad1bbc959ea4
TecXCTO <cto.tecx@gmail.com>
2026-01-19 17:40:30 +0530

    Fix docstring formatting in feedforward method

--- src/nn.py
+++ src/nn.py
@@ -2,6 +2,7 @@ class Network(object):
   def _init__(self, *args, **kwargs):
     #...yada yada, initialize weights and biases...
   def feedforward(self, a):
-    """Return the output of the network for an input vector a""n for b, w in zip(self.biases, self.weights):
-    a= sigmoid(np.dot(w, a)+b)
+    """Return the output of the network for an input vector a"""
+    for b, w in zip(self.biases, self.weights):
+      a= sigmoid(np.dot(w, a)+b)
     return a
