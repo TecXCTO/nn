d563a8db90cb542f9950a16988e46891110203c1
TecXCTO <cto.tecx@gmail.com>
2026-01-19 12:01:46 +0530

    Create nn.py

---
+++ src/nn.py
@@ -0,0 +1,6 @@
+class Network(object)：
+def _init__(self, *args, **kwargs):
+#...yada yada, initialize weights and biases...def feedforward(self, a):
+"""Return the output of the network for an input vector a""n for b, w in zip(self.biases, self.weights):
+a= sigmoid(np.dot(w, a)+b)
+return a
